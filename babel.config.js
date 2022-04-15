70dedfd7db3cc959e7702137363471fee74abdcc
Anton Gilgur <agilgur5@gmail.com>
2022-04-15 16:02:11 -0400

    types: fix type errors in babel.config.js
    - forgot to run `tsc` after adding `@tsconfig/strictest` 😅
      - will need to add some CI and maybe pre-commit hooks to this later!
      - this added `checkJs`, so now it's checked!
    
    - can't use `babel.config.ts` right now, but can use JSDoc types in the
      meantime
      - couldn't find `ConfigFunction` in exported in the source, but did
        find it in `@types/babel__core`, which is an existing transitive dep
        - I think `@babel/traverse` might use it?
      - c.f. my comment in https://github.com/babel/babel/issues/8529#issuecomment-1100335605
        for `babel.config.ts` reference. was going to ask about if anyone
        knew the config's typings for JSDoc, but then figured I could find
        it myself

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,11 @@
+/** @type {import('@babel/core').ConfigFunction} */
 module.exports = api => {
-  api.cache.using(() => process.env.NODE_ENV) // cache based on NODE_ENV
+  api.cache.using(() => process.env['NODE_ENV']) // cache based on NODE_ENV
 
   // normally use browserslistrc, but for Jest, use current version of Node
   const isTest = api.env('test')
   const jestTargets = { targets: { node: 'current' } }
+  /** @type {import('@babel/core').PluginItem} */
   let presetEnv = '@babel/preset-env'
   if (isTest) presetEnv = [presetEnv, jestTargets]
 
